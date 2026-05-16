a6f6d2594bbf6010c8a938fc0c7cea63c3ff6686
jkosimov806-creator <jkosimov806@gmail.com>
2026-05-16 12:28:30 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 80;
+
+const server = http.createServer((req, res) => {
+    // Определяем путь к запрашиваемому файлу
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    let extname = path.extname(filePath);
+    
+    // Задаем правильный тип контента
+    let contentType = 'text/html';
+    if (extname === '.css') contentType = 'text/css';
+    if (extname === '.js') contentType = 'text/javascript';
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Файл не найден', 'utf-8');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
 
