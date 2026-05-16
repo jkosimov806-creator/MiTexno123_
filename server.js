61608b06c13067471cc5979532f3c028d731a146
jkosimov806-creator <jkosimov806@gmail.com>
2026-05-16 21:12:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
 const mime = {'.html':'text/html','.css':'text/css','.js':'application/javascript','.png':'image/png','.jpg':'image/jpeg','.ico':'image/x-icon'};
 http.createServer((req, res) => {
   let file = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
@@ -16,4 +17,4 @@ http.createServer((req, res) => {
     res.writeHead(200,{'Content-Type': mime[path.extname(file)]||'text/plain'});
     res.end(data);
   });
-}).listen(PORT, ()=>console.log('Mi Texno running on port '+PORT));
+}).listen(PORT, HOST, ()=>console.log('Mi Texno running on '+HOST+':'+PORT));
