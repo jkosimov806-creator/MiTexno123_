631910b8daac9afe0dc4188ed801152585219d12
jkosimov806-creator <jkosimov806@gmail.com>
2026-05-16 20:59:26 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
-const mime = {'.html':'text/html','.css':'text/css','.js':'application/javascript'};
+const mime = {'.html':'text/html','.css':'text/css','.js':'application/javascript','.png':'image/png','.jpg':'image/jpeg','.ico':'image/x-icon'};
 http.createServer((req, res) => {
   let file = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
   fs.readFile(file, (err, data) => {
-    if (err) { fs.readFile(path.join(__dirname,'index.html'),(_,d)=>{ res.writeHead(200,{'Content-Type':'text/html'}); res.end(d); }); return; }
+    if (err) {
+      fs.readFile(path.join(__dirname,'index.html'),(_,d)=>{
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(d);
+      });
+      return;
+    }
     res.writeHead(200,{'Content-Type': mime[path.extname(file)]||'text/plain'});
     res.end(data);
   });
-}).listen(PORT, ()=>console.log('OK port '+PORT));
+}).listen(PORT, ()=>console.log('Mi Texno running on port '+PORT));
